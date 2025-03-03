be2acb46427880611b0972256f47488bd51baf38
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-03 17:51:38 +0530

    dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,30 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import NextTopLoader from "nextjs-toploader";
 import Footer from "@/components/Footer";
+import QueryProvider from "@/utils/QueryProvider";
 
 export const metadata: Metadata = {
-  title: "Clozit | Elevate Your Style, Define Your Story",
-  description: "Shop the latest fashion trends at Clozit – your go-to online clothing store for stylish, high-quality apparel. Discover trendy outfits, exclusive collections, and effortless shopping with fast delivery. Upgrade your wardrobe today!",
+    title: "Clozit | Elevate Your Style, Define Your Story",
+    description: "Shop the latest fashion trends at Clozit – your go-to online clothing store for stylish, high-quality apparel. Discover trendy outfits, exclusive collections, and effortless shopping with fast delivery. Upgrade your wardrobe today!",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <NextTopLoader />
-        <Navbar></Navbar>
-        {children}
-        <Footer></Footer>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body>
+        <QueryProvider>
+            <NextTopLoader/>
+            <Navbar></Navbar>
+            {children}
+            <Footer></Footer>
+        </QueryProvider>
+        </body>
+        </html>
+    );
 }
