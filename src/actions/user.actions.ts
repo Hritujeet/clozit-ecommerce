00e82b55c99d071c7e64fe93d084cb1343883be3
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-04 11:44:52 +0530

    dev

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,24 +1,25 @@
 "use server";
 import {signIn, signOut} from "@/auth";
-import { Credentials } from "@/utils/types";
+import {Credentials} from "@/utils/types";
 
 export async function credentialSignInHandler(data: Credentials) {
     try {
-        const credentials = { email: data.email, password: data.password };
+        const credentials = {email: data.email, password: data.password};
         await signIn("credentials", {
             ...credentials,
             redirect: false
         });
 
-        return null; 
-    } catch (error: any) {
-        console.log(error); 
+        return null;
+    }
+    catch (error: any) {
+        console.log(error);
         if (error.cause && error.cause.err) {
             return error.cause.err.message;
         } else if (error.message) {
-            return error.message; 
+            return error.message;
         } else {
-            return "An unknown error occurred."; 
+            return "An unknown error occurred.";
         }
     }
 }
