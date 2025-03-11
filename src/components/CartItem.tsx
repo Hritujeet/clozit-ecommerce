7d7bb36cc135cfb7cc91127af265610dfd096cf7
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-11 09:45:17 +0530

    dev

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,33 +1,61 @@
-import React from 'react';
-import {Button} from "@/components/ui/button";
-import {MinusCircle, PlusCircle} from "lucide-react";
+"use client"
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { MinusCircle, PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "@/redux/slice/counter";
+import { RootState } from "@/redux/store";
 
 const CartItem = () => {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
         <div className="flex w-full items-center space-x-2 sm:space-x-4">
-            <Image width={100} height={100}
+            <Image
+                width={100}
+                height={100}
                 className="flex-shrink-0 object-cover w-20 h-20 dark:border- rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500"
                 src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80"
-                alt="Polaroid camera"/>
+                alt="Polaroid camera"
+            />
             <div className="flex flex-col justify-between w-full pb-4">
                 <div className="flex justify-between w-full pb-2 space-x-2">
                     <div className="space-y-1">
-                        <h3 className="text-lg font-semibold leading-snug sm:pr-8">Polaroid camera</h3>
+                        <h3 className="text-lg font-semibold leading-snug sm:pr-8">
+                            Polaroid camera
+                        </h3>
                         <p className="text-sm dark:text-gray-600">Classic</p>
                     </div>
                     <div className="text-right">
                         <p className="text-lg font-semibold">59.99€</p>
-                        <p className="text-sm line-through dark:text-gray-400">75.50€</p>
+                        <p className="text-sm line-through dark:text-gray-400">
+                            75.50€
+                        </p>
                     </div>
                 </div>
                 <div className="flex text-sm gap-3 items-center">
-                    <Button variant={"secondary"} size={"icon"} className="flex justify-center items-center px-2 py-1">
-                        <PlusCircle/>
+                    <Button
+                        variant={"secondary"}
+                        size={"icon"}
+                        className="flex justify-center items-center px-2 py-1"
+                        onClick={()=>{
+                            dispatch(increment())
+                        }}
+                    >
+                        <PlusCircle />
                     </Button>
-                    <span className={"font-semibold"}>2</span>
-                    <Button variant={"secondary"} size={"icon"} className="flex justify-center items-center px-2 py-1">
-                        <MinusCircle/>
+                    <span className={"font-semibold"}>{count}</span>
+                    <Button
+                        variant={"secondary"}
+                        size={"icon"}
+                        className="flex justify-center items-center px-2 py-1"
+                        onClick={()=>{
+                            dispatch(decrement())
+                        }}
+                    >
+                        <MinusCircle />
                     </Button>
                 </div>
             </div>
--- src/utils/QueryProvider.tsx
+++ src/utils/QueryProvider.tsx
@@ -1,16 +1,24 @@
-"use client"
-import React from 'react';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {SessionProvider} from "next-auth/react";
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 
-const QueryProvider = ({children}: { children: Readonly<React.ReactNode> }) => {
+const QueryProvider = ({
+    children,
+}: {
+    children: Readonly<React.ReactNode>;
+}) => {
     const client = new QueryClient();
     return (
-        <SessionProvider>
-            <QueryClientProvider client={client}>
-                {children}
-            </QueryClientProvider>
-        </SessionProvider>
+        <Provider store={store}>
+            <SessionProvider>
+                <QueryClientProvider client={client}>
+                    {children}
+                </QueryClientProvider>
+            </SessionProvider>
+        </Provider>
     );
 };
 
