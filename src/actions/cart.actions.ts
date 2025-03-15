43dbdd4d850b964d108e9258d5e3f0c3cba56ca3
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-15 15:10:39 +0530

    dev

--- src/actions/cart.actions.ts
+++ src/actions/cart.actions.ts
@@ -11,6 +11,8 @@ export const addToCartServer = async (data: CartDataServer) => {
 
         // Find user model to get user id
         const user = await User.findOne({email: data.email});
+
+        // Check if a cart exists for the current user. If it doesn't, create a new cart
         let cart = await Cart.findOne({user: user._id}).populate("items");
         if (!cart) {
             cart = new Cart({
@@ -31,6 +33,7 @@ export const addToCartServer = async (data: CartDataServer) => {
             }, // Ensures variant uniqueness
         });
 
+        // If the product already exists as a cartItem, then increment its quantity. Otherwise, create a new item in our cart
         if (existingItem) {
             existingItem.quantity += 1;
             await existingItem.save();
@@ -52,6 +55,7 @@ export const addToCartServer = async (data: CartDataServer) => {
         const updatedItems = await CartItem.find({cart: cart._id});
         cart.subtotal = updatedItems.reduce((sum, item) => sum + item.quantity * data.price, 0);
 
+        // save the cart ta last to reflect all the changes in the database
         await cart.save();
 
         return {success: true};
