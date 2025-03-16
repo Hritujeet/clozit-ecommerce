2778573e41d18da5cab0131beec2916303834bb7
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-16 11:21:12 +0530

    dev

--- src/utils/cart-client.ts
+++ src/utils/cart-client.ts
@@ -2,14 +2,32 @@
 
 import {CartDataClient} from "@/utils/types";
 
-export const addToCartClient = (data: CartDataClient) => {
-    const localCart: CartDataClient[] = JSON.parse(localStorage.getItem("Cart") || "[]");
-    localCart.push({...data});
-    localStorage.setItem("Cart", JSON.stringify(localCart));
+type CartElement = {
+    product: CartDataClient,
+    qty: number
+}
 
+export const addToCartClient = (data: CartDataClient) => {
+    const localCart: CartElement[] = JSON.parse(localStorage.getItem("Cart") || "[]");
+    
+    const existingItem = localCart.find((element: CartElement) => 
+        element.product.name === data.name &&
+        element.product.size === data.size &&
+        element.product.color === data.color
+    );
+    
+    if (existingItem) {
+        existingItem.qty += 1;
+    } else {
+        localCart.push({
+            product: data,
+            qty: 1
+        });
+    }
+    
+    localStorage.setItem("Cart", JSON.stringify(localCart));
     console.log("Updated Cart:", localCart);
 };
-
 
 export const removeFromCartClient = () => {
 
