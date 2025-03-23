1a76649ef788c1a84671bd1979fe08a8099fc750
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-23 16:13:15 +0530

    dev

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,25 +1,8 @@
 import mongoose from "mongoose";
 
-const DB_URI = process.env.DB_URI;
-
-export const connectDb = async () => {
-    try {
-        if (mongoose.connection.readyState) {
-            console.log("Already Connected");
-            return;
-        }
-
-        mongoose.set("strictQuery", false); // Disable strict query mode
-        mongoose.set("bufferCommands", false); // Prevent buffering
-
-        await mongoose.connect(DB_URI);
-
-        console.log("Database Connected Successfully");
-    } catch (error) {
-        console.error("MongoDB Connection Error:", error);
+export async function connectDb() {
+    if (mongoose.connections && mongoose.connections[0].readyState) {
+        return
     }
-};
-
-mongoose.connection.on("connected", () => console.log("MongoDB connected"));
-mongoose.connection.on("error", (err) => console.log("MongoDB error:", err));
-mongoose.connection.on("disconnected", () => console.log("MongoDB disconnected"));
+    await mongoose.connect(process.env.MONGO_URI);
+}
