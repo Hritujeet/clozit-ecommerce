167b4b02056fc46782ee4b048cc62f4321ea3966
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-23 16:17:58 +0530

    dev

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import { DB_URI } from "./types";
 
 export async function connectDb() {
+    console.log(DB_URI);
     if (mongoose.connections && mongoose.connections[0].readyState) {
         return
     }
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(DB_URI);
 }
