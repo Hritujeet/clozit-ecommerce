ab610e8c8ea7880526d02bc66a35fc301c1a779d
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-23 15:52:18 +0530

    dev

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,13 @@ const nextConfig: NextConfig = {
           protocol : "https",
           hostname : "*",
       }]
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
 };
 
 export default nextConfig;
