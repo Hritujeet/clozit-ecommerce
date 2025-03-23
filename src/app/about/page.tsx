f58714a58c46659117903cef9e75f12c52244b50
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-03-23 14:46:58 +0530

    dev

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const Page = () => {
     return (
-        <div>
-            About
-        </div>
+        <section className="text-gray-600 body-font">
+            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                <div className="text-center lg:w-2/3 w-full">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 flex flex-col">
+                        <span>Clozit Ecommerce - Ecommerce Site Prototype By</span> <Link className="hover:underline" href={"https://github.com/hritujeet"}>@Hritjeet Sharma</Link>
+                    </h1>
+                    <p className="mb-8 leading-relaxed">
+                       Clozit | Ecommerce is a basic ecommerce clothing store website as a prototype of an ecommerce store. This site is created using NextJS, Typescript, MongoDB with Mongoose and Tanstack Query. This website is not a real ecommerce clothing store and as such doesn't incoporate any real stock of products displayed and payment gateways. You can freelly place order without having to pay as this is a demo (prototype) pf a bigger idea. Feel Free to Contact me for any suggestions about this project. This is a part of my learnings.
+                    </p>
+                    <div className="flex justify-center">
+                        <Link href={"/"} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Home
+                        </Link>
+                        <Link href={"/products"} className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                            Products
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,5 @@
 import {auth} from '@/auth';
 import React from 'react';
-import {BsCart} from "react-icons/bs";
 import Link from "next/link";
 import {buttonVariants} from "@/components/ui/button";
 
@@ -9,21 +8,7 @@ const Page = async () => {
     console.log(authData);
 
     return (
-        <div className='flex flex-col gap-4 w-[80vw] md:w-[50vw] mt-10 mx-auto'>
-            <div className="overview grid grid-cols-1 gap-2">
-                <div
-                    className="card flex flex-col justify-center items-center px-8 py-4 border rounded-md cursor-default gap-2">
-                   <span className="text-lg text-neutral-600 flex gap-2 justify-center items-center">
-                       <BsCart className={"text-2xl"}/> Orders Made
-                   </span>
-                    <span className="text-4xl font-bold">20</span>
-                </div>
-            </div>
-            <div className="grid grid-cols-1 1ap-2 md:w-[50vw] w-[80vw] mx-auto">
-                <Link href={"/profile/orders"}
-                      className={`font-semibold ${buttonVariants({size: "lg", variant: "default"})}`}>Orders</Link>
-            </div>
-
+        <div className='flex flex-col gap-4 w-[80vw] md:w-[50vw] mt-10 mx-auto py-24'>
             <div className="py-4 md:py-8">
                 <div className="mb-4 grid gap-4 sm:grid-cols-2 sm:gap-8 lg:gap-16">
                     <div className="space-y-4">
@@ -40,6 +25,11 @@ const Page = async () => {
                     </div>
                 </div>
             </div>
+            <div className="grid grid-cols-1 1ap-2 md:w-[50vw] w-[80vw] mx-auto">
+                <Link href={"/profile/orders"}
+                      className={`font-semibold ${buttonVariants({size: "lg", variant: "default"})}`}>Orders</Link>
+            </div>
+
         </div>
     );
 };
