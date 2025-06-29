048a3728d044bd7f4e7ff25f8214eca9c9c58616
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-06-29 14:17:19 +0530

    dev

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
-import type { NextConfig } from "next";
+import {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "*",
-            },
-        ],
-    },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         ignoreBuildErrors: true,
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*',
+                port: '',
+                pathname: '/**',
+            }
+        ]
+    }
 };
 
-export default nextConfig;
+export default nextConfig
